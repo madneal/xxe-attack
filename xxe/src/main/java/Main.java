b7ced49563d3563f753430d2e4e3739926311ac1
neal1991 <neal1991@sina.com>
2020-06-10 17:02:13 +0800

    add check for unzip

--- xxe/src/main/java/Main.java
+++ xxe/src/main/java/Main.java
@@ -1,14 +1,20 @@
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class Main {
     public static void main(String[] args) {
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-        ReadFromXml(classLoader.getResource("payload1.xml").getFile());
+//        ReadFromXml(classLoader.getResource("payload1.xml").getFile());
+        try {
+
+            unzip(classLoader.getResource("payload.xml.zip").getFile());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void ReadFromXml(String filepath) {
@@ -30,6 +36,51 @@ public class Main {
             builder.parse(is);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void unzip(String filename) throws java.io.IOException {
+        FileInputStream fis = new FileInputStream(filename);
+        ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
+        ZipEntry entry;
+        int entries = 0;
+        long total = 0;
+        try {
+            while ((entry = zis.getNextEntry()) != null) {
+                System.out.println("Extracting: " + entry);
+
+                File f = new File("/Users/dongbing" + "/" + "../../../../etc/passwd");
+                System.out.println(f.getAbsolutePath());
+                int count;
+//                byte data[] = new byte[BUFFER];
+                // Write the files to the disk, but ensure that the filename is valid,
+                // and that the file is not insanely big
+                String name = validateFilename(entry.getName(), ".");
+                if (entry.isDirectory()) {
+                    System.out.println("Creating directory " + name);
+                    new File(name).mkdir();
+                    continue;
+                }
+                FileOutputStream fos = new FileOutputStream(name);
+
+            }
+        } finally {
+            zis.close();
+        }
+    }
+
+    private static String validateFilename(String filename, String intendedDir)
+            throws java.io.IOException {
+        File f = new File(filename);
+        String canonicalPath = f.getCanonicalPath();
+
+        File iD = new File(intendedDir);
+        String canonicalID = iD.getCanonicalPath();
+
+        if (canonicalPath.startsWith(canonicalID)) {
+            return canonicalPath;
+        } else {
+            throw new IllegalStateException("File is outside extraction target directory.");
         }
     }
 }
